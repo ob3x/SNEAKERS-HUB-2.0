60d09490ca1d7ff1e4060ca67e58ca1c2ab5c679
Szymon <ob3x.biznes@gmail.com>
2024-04-16 20:28:59 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,23 @@ const currentDate = new Date();
 const sizesBtn = document.querySelector('.item-box__right-sizes');
 const sizesEl = document.querySelector('.item-box__right-sizes-box');
 const arrow = document.querySelector('.item-box__right-sizes--title-icon');
+const sizeItems = document.querySelectorAll('.item-box__right-sizes-box--item');
+const sizeTitle = document.querySelector('.item-box__right-sizes--title-text');
 
 const heartFunction = (e) => {
 	e.target.classList.toggle('heart-active');
 };
-
+const changeTitle = (e) => {
+    sizeTitle.textContent = e.target.textContent
+}
 
 hearts.forEach((heart) => {
     heart.addEventListener('click', heartFunction);
 });
+
+sizeItems.forEach(item => {
+    item.addEventListener('click', changeTitle)
+})
 
 sizesBtn.addEventListener('click', () => {
     sizesEl.classList.toggle('sizes-active')
