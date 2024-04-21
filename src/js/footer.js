06da10290bb7afb5bde75aa7abfc0fbff9bbf853
Szymon <ob3x.biznes@gmail.com>
2024-04-21 19:38:37 +0200

    update

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,3 +1,10 @@
 const footerYear = document.querySelector('.footer-bottom__year');
+const cookies = document.querySelector('.cookies');
+const cookiesBtn = document.querySelector('.cookies-box__btn');
 const currentDate = new Date();
+
+cookiesBtn.addEventListener('click', () => {
+	cookies.classList.add('active');
+});
+
 footerYear.textContent = currentDate.getFullYear();
